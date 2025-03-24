73bbb494b8d5301ef78afa72dd181b3372450775
kys-rajat <152905656+kys-rajat@users.noreply.github.com>
2025-03-24 20:27:09 +0530

    Create cpu_scheduler_Priority_Scheduling.py

---
+++ cpu_scheduler_Priority_Scheduling.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+def input_processes():
+    processes = []
+    num_processes = int(input("Enter the number of processes: "))
+    for i in range(num_processes):
+        arrival_time = int(input(f"Enter arrival time for process {i+1}: "))
+        burst_time = int(input(f"Enter burst time for process {i+1}: "))
+        priority = int(input(f"Enter priority for process {i+1}: "))
+        processes.append({
+            "id": i + 1,
+            "arrival_time": arrival_time,
+            "burst_time": burst_time,
+            "priority": priority
+        })
+    return processes
